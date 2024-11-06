7bf69df6d465a78a55036c4cd5133f16aa8a82a5
seniya <seniyathri@gmail.com>
2024-11-06 21:55:57 +0100

    test

---
+++ src/it/check.java
@@ -0,0 +1,7 @@
+package it;
+
+public class check {
+  public static void main(String[] args) {
+    System.out.println("hello");
+  }
+}
---
+++ src/it/test.java
@@ -0,0 +1,18 @@
+package it;
+
+public class test {
+  public static void main(String[] args) {
+    System.out.println("hello-world");
+
+    System.out.println("this is mt first project");
+
+    System.out.println("this i a test to page");
+    System.out.println("this i a test to page");
+    System.out.println("this i a test to page");
+    System.out.println("this i a test to page");
+    
+
+   
+    
+  }
+}
